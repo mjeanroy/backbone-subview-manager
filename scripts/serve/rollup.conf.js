07b2638231df59f46e0d36cb6a1e4004b0bc1aa0
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2019-08-08 14:16:35 +0200

    chore: update usage of rollup-plugin-node-resolve

--- scripts/serve/rollup.conf.js
+++ scripts/serve/rollup.conf.js
@@ -18,8 +18,11 @@ module.exports = {
     }),
 
     nodeResolve({
-      jsnext: true,
-      main: true,
+      mainFields: [
+        'module',
+        'jsnext:main',
+        'main',
+      ],
     }),
 
     commonjs(),
